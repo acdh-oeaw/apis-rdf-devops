bf2b4c719311b9008898414c755882819eb1ef12
gythaogg <gythaoggscat@gmail.com>
2022-12-05 04:23:02 +0100

    add template folder to override from apis-ontologies in settings

--- apis/settings/base.py
+++ apis/settings/base.py
@@ -174,7 +174,7 @@ ROOT_URLCONF = "apis.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "apis_ontology", "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
