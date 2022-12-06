db3f63bbcb5ac4e07c8332e0adcbe55c1b696a90
KK <dev-kk@oeaw.ac.at>
2022-12-06 13:57:53 +0100

    Adapt start script, stdout more info

--- start.sh
+++ start.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
-echo "Django instance for project: $APIS_RDF_ONTOLOGY"
+echo "Django instance for: $APIS_RDF_ONTOLOGY ontology"
 echo "Current dir: $(pwd)"
-ls -la
-pwd
+echo "Dir contents:\n$(ls -la)"
+echo "Creating symlink to ontology..."
 rm -f apis_ontology
 ln -s apis-ontologies/$APIS_RDF_ONTOLOGY apis_ontology
-ls -la
-pwd
-echo $APIS_RDF_ONTOLOGY
+echo "$(file apis_ontology)"
+echo "Running Django management commands:"
+echo "Migrating..."
 python manage.py migrate
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
 #ls /var/solr_new/paas_solr
 #python manage.py build_solr_schema --configure-directory /var/solr_new/paas_solr/conf --reload-core default
