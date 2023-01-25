f843e591dc8647fce447e53e29799fae81ddc373
sennierer <matthias.schloegl@oeaw.ac.at>
2023-01-25 17:23:19 +0100

    adds a possibility to install sub-groups via env
    poetry allows for installing of sub-groups. setting the env variable POETRY_ADDITIONAL_GROUPS to comma separated list of groups will install those groups on top of the libs installed in the base image. E.. POETRY_ADDITIONAL_GROUPS=dev,frischmuth will install dev and frischmuth group on startup of the server

--- start.sh
+++ start.sh
@@ -16,6 +16,10 @@ python manage.py collectstatic --noinput
 #ls /var/solr_new/paas_solr
 #python manage.py build_solr_schema --configure-directory /var/solr_new/paas_solr/conf --reload-core default
 #gunicorn apis.wsgi --timeout 120 --workers=3 --threads=3 --worker-connections=1000
+if [[ $POETRY_ADDITIONAL_GROUPS ]]; then
+    echo "installing additional groups $POETRY_ADDITIONAL_GROUPS"
+    poetry install --only $POETRY_ADDITIONAL_GROUPS
+fi
 if [[ -z "${DEVELOP}" ]]; then
     echo "starting gunicorn"
     gunicorn apis.wsgi -b 0.0.0.0:5000 --timeout 120 --workers=3 --threads=3 --worker-connections=1000
