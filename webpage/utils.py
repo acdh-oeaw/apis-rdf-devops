33a0b2e402e3ca343b4d29ab152bb5fb14bd12db
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-03-06 14:16:29 +0100

    Remove integration of external metadata
    Closes: acdh-oeaw/apis-core-rdf#50

--- webpage/utils.py
+++ webpage/utils.py
@@ -20,19 +20,8 @@ try:
 except AttributeError:
     PROJECT_CSS = "{}/style.css".format(PROJECT_STATIC_URL)
 
-
-def get_project_md():
-    try:
-        r = requests.get(PROJECT_MD_URL)
-    except Exception as e:
-        print(e)
-        return settings.PROJECT_DEFAULT_MD
-    if r.status_code == 200:
-        return r.json()
-    else:
-        return settings.PROJECT_DEFAULT_MD
-
-
-PROJECT_METADATA = get_project_md()
+PROJECT_METADATA = {}
+if getattr(settings, "PROJECT_DEFAULT_MD"):
+    PROJECT_METADATA = settings.PROJECT_DEFAULT_MD
 
 SHARED_URL = settings.SHARED_URL
