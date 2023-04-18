692f97b3a3b2cb879e5cfe60fdac1c8245915b99
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2023-04-18 09:02:25 +0200

    fix: updates urls in webpage module to path

--- webpage/urls.py
+++ webpage/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import path, re_path
 from django.views.generic.base import RedirectView
 from . import views
 
@@ -7,13 +7,13 @@ app_name = 'webpage'
 favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
-    url(r'^favicon\.ico$', favicon_view),
-    url(r'^imprint', views.ImprintView.as_view(), name="imprint"),
-    url(r'^$', views.GenericWebpageView.as_view(), name="start"),
-    url(r'^accounts/login/$', views.user_login, name='user_login'),
-    url(r'^logout/$', views.user_logout, name='user_logout'),
-    url(r'^set_user_settings/$', views.set_user_settings, name='set_user_settings'),
-    url(r'^project-info/$', views.project_info, name='project_info'),
-    url(r'^(?P<template>[\w-]+)/$', views.GenericWebpageView.as_view(), name='staticpage'),
+    path('favicon.ico', favicon_view),
+    path(r'imprint', views.ImprintView.as_view(), name="imprint"),
+    path('', views.GenericWebpageView.as_view(), name="start"),
+    path('accounts/login/', views.user_login, name='user_login'),
+    path('logout/', views.user_logout, name='user_logout'),
+    path('set_user_settings/', views.set_user_settings, name='set_user_settings'),
+    path('project-info/', views.project_info, name='project_info'),
+    re_path(r'^(?P<template>[\w-]+)/$', views.GenericWebpageView.as_view(), name='staticpage'),
 
 ]
